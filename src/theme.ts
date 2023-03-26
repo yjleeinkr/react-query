0c9f629e05634dbb65f9c894598bbcb984c0a456
yjleeinkr <lucy.lee930216@gmail.com>
2023-03-27 01:29:51 +0900

    init / declare global style and theme using styled-components

---
+++ src/theme.ts
@@ -0,0 +1,13 @@
+import { DefaultTheme } from "styled-components"
+
+export const theme: DefaultTheme = {
+  textColor: "#f5f6fa",
+  bgColor: "#2f3640",
+  accentColor: "#844C5F",
+};
+
+// export const darkTheme: DefaultTheme = {
+//   textColor: "white",
+//   bgColor: "black",
+//   btnColor: "teal",
+// };
