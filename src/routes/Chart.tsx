917380ea6440ee75793c1d878e62e5678913ea2e
yjleeinkr <lucy.lee930216@gmail.com>
2023-03-30 01:22:47 +0900

    create chart with apexchart

--- src/routes/Chart.tsx
+++ src/routes/Chart.tsx
@@ -1,5 +1,68 @@
-function Chart() {
-    return <h1>Chart</h1>;
+import { useQuery } from "react-query";
+import { fetchCoinHistory } from "../api";
+import ApexChart from 'react-apexcharts'
+interface HistoricalData {
+    time_open: number;
+    time_close: number;
+    open:  string;
+    high:  string;
+    low: string;
+    close: string;
+    volume : string;
+    market_cap: number;
+}
+interface ChartProps { 
+    coinId: string;
+}
+function Chart({ coinId }: ChartProps) {
+    const { isLoading, data } = useQuery<HistoricalData[]>( ["ohlcv", coinId],() => fetchCoinHistory(coinId), { refetchInterval: 10000 });
+    
+    return (
+      <div>
+        {isLoading ? (
+          "Loading chart..."
+        ) : Array.isArray(data) ? (
+          <ApexChart
+            type="line"
+            series={[
+              {
+                name: "Price",
+                data: data?.map((price) => Number(price.close)) ?? [],
+              },
+            ]}
+            options={{
+              chart: {
+                height: 300,
+                width: 500,
+                toolbar: { show: false },
+                background: "transparent"
+                },
+              grid: { show: false },
+              theme: { mode: "dark" },
+              stroke: { curve: "smooth", width: 3},
+              yaxis: { show: false },
+              xaxis: {
+                labels: { show: false},
+                axisBorder: { show:false },
+                axisTicks: { show: false},
+                categories: data?.map(price => price.time_close),
+                type: "datetime"
+              },
+              fill: {
+                type: "gradient",
+                gradient: { gradientToColors: ['#0be881'], stops: [0,100] },
+               },
+               colors: ['#0fbcf9'],
+               tooltip: {
+                  y: { formatter: (value)=> `$ ${value.toFixed(2)}`}
+                }
+            }}
+          />
+        ) : (
+          "Oops, something went wrong! Please try again later."
+        )}
+      </div>
+    );
 }
 
 export default Chart;
